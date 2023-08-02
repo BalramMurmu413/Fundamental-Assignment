0c7b308302bf945fe8facefb09d1b7a0ad6c450c
Balram Murmu <123802234+BalramMurmu413@users.noreply.github.com>
2023-08-02 21:19:00 +0530

    Add files via upload

---
+++ Calculates Rentle Cost/calculateRentleCost.js
@@ -0,0 +1,21 @@
+
+
+function luxury(p){
+    return p*20000
+  }
+  
+  function midsize(p){
+    return p*10000
+  }
+  
+  function economy(p){
+    return p*4000
+  }
+  
+    
+  console.log(luxury(2))
+  console.log(economy(2))
+  console.log(midsize(2))
+  
+    
+  
