7c99e02358afb708e2bc4e10a64432d553ca1a72
Balram Murmu <123802234+BalramMurmu413@users.noreply.github.com>
2023-08-20 23:28:05 +0530

    Update calculateCarRentleCost.js

--- Calculates Rentle Cost/calculateRentleCost.js
+++ Calculates Rentle Cost/calculateRentleCost.js
@@ -1,21 +1,26 @@
+function rentalCost(days,  carType){
+    let economy = 4000
+    let luxury = 20000
+    let midsize = 10000
+    switch (carType) {
+    case "luxury":
+        console.log(days * luxury)
+        break;
+        case "midsize":
+        console.log(days * midsize)
+        break;
+        case "economy":
+            console.log(days * economy)
+            break;
+        default:
+        console.log("Please choose valid car or days")
+        break;
+}}
 
 
-function luxury(p){
-    return p*20000
-  }
-  
-  function midsize(p){
-    return p*10000
-  }
-  
-  function economy(p){
-    return p*4000
-  }
-  
-    
-  console.log(luxury(2))
-  console.log(economy(2))
-  console.log(midsize(2))
-  
+console.log(rentalCost(2,"midsize"))
+console.log(rentalCost(2,"luxury"))
+console.log(rentalCost(3,"economy"))
+
     
   
