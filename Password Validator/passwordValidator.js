ebf8364ca48e17e9ac24c644f95e3f192cbc241f
Balram Murmu <123802234+BalramMurmu413@users.noreply.github.com>
2023-08-20 23:31:13 +0530

    Add files via upload

---
+++ Password Validator/passwordValidator.js
@@ -0,0 +1,9 @@
+
+let password = "Abc@pW"
+let confirmPassword = "Abc@pW"
+
+if (password === confirmPassword){
+    console.log("Password Matched. Password Validate Successful ");
+}else{
+    console.log("Password did'n Matched. Password Validate Unsuccessful");
+}
